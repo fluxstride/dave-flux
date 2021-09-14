5bc44a99f97d7774449102e9a56877de3e95d269
adekoya samuel <adekoyasamuel52@gmail.com>
2021-09-14 03:32:17 +0100

    init

--- components/sharedFiles/json.js
+++ components/sharedFiles/json.js
@@ -1,32 +1,32 @@
 
-const sharedFiles = [
+const images = [
     {
-        id: 1, name: "Design 101", sharedBy: "BJTone", shareDate: "12/04/20", fileSize: "180 KB", path: { userPath: "/images/icon.svg" }
+        id: 1, name: "Design 101", sharedBy: "BJTone", shareDate: "12/04/20", fileSize: "180 KB", path :{userPath: "/images/user.png",iconPath:"/images/icon.svg"}
     },
     {
-        id: 2, name: "Design 101", sharedBy: "BJTone", shareDate: "12/04/20", fileSize: "180 KB", path: { userPath: "/images/icon.svg" }
+        id: 2, name: "Design 101", sharedBy: "BJTone", shareDate: "12/04/20", fileSize: "180 KB", path :{userPath: "/images/user.png",iconPath:"/images/icon.svg"}
     },
     {
-        id: 3, name: "Design 101", sharedBy: "BJTone", shareDate: "12/04/20", fileSize: "180 KB", path: { userPath: "/images/icon.svg" }
+        id: 3, name: "Design 101", sharedBy: "BJTone", shareDate: "12/04/20", fileSize: "180 KB", path :{userPath: "/images/user.png",iconPath:"/images/icon.svg"}
     },
     {
-        id: 4, name: "Design 101", sharedBy: "BJTone", shareDate: "12/04/20", fileSize: "180 KB", path: { userPath: "/images/icon.svg" }
+        id: 4, name: "Design 101", sharedBy: "BJTone", shareDate: "12/04/20", fileSize: "180 KB", path :{userPath: "/images/user.png",iconPath:"/images/icon.svg"}
     },
     {
-        id: 5, name: "Design 101", sharedBy: "BJTone", shareDate: "12/04/20", fileSize: "180 KB", path: { userPath: "/images/icon.svg" }
+        id: 5, name: "Design 101", sharedBy: "BJTone", shareDate: "12/04/20", fileSize: "180 KB", path :{userPath: "/images/user.png",iconPath:"/images/icon.svg"}
     },
     {
-        id: 6, name: "Design 101", sharedBy: "BJTone", shareDate: "12/04/20", fileSize: "180 KB", path: { userPath: "/images/icon.svg" }
+        id: 6, name: "Design 101", sharedBy: "BJTone", shareDate: "12/04/20", fileSize: "180 KB", path :{userPath: "/images/user.png",iconPath:"/images/icon.svg"}
     },
     {
-        id: 7, name: "Design 101", sharedBy: "BJTone", shareDate: "12/04/20", fileSize: "180 KB", path: { userPath: "/images/icon.svg" }
+        id: 7, name: "Design 101", sharedBy: "BJTone", shareDate: "12/04/20", fileSize: "180 KB", path :{userPath: "/images/user.png",iconPath:"/images/icon.svg"}
     },
     {
-        id: 8, name: "Design 101", sharedBy: "BJTone", shareDate: "12/04/20", fileSize: "180 KB", path: { userPath: "/images/icon.svg" }
+        id: 8, name: "Design 101", sharedBy: "BJTone", shareDate: "12/04/20", fileSize: "180 KB", path :{userPath: "/images/user.png",iconPath:"/images/icon.svg"}
     },
-
-
+  
+   
 ]
 
 
-export default sharedFiles;
+export default images;
