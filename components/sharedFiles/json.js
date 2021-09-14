8c9194dfe89b744eec5e5796ad48fcf2d4f2bb3e
adekoya samuel <adekoyasamuel52@gmail.com>
2021-09-14 03:10:40 +0100

    init

--- components/sharedFiles/json.js
+++ components/sharedFiles/json.js
@@ -1,31 +1,31 @@
 
 const sharedFiles = [
     {
-        id: 1, name: "Design 101", sharedBy: "BJTone", shareDate: "12/04/20", fileSize: "180 KB", path :{userPath: "/sharedFiles/user.png",iconPath:"/sharedFiles/icon.svg"}
+        id: 1, name: "Design 101", sharedBy: "BJTone", shareDate: "12/04/20", fileSize: "180 KB", path: { userPath: "/images/icon.svg" }
     },
     {
-        id: 2, name: "Design 101", sharedBy: "BJTone", shareDate: "12/04/20", fileSize: "180 KB", path :{userPath: "/sharedFiles/user.png",iconPath:"/sharedFiles/icon.svg"}
+        id: 2, name: "Design 101", sharedBy: "BJTone", shareDate: "12/04/20", fileSize: "180 KB", path: { userPath: "/images/icon.svg" }
     },
     {
-        id: 3, name: "Design 101", sharedBy: "BJTone", shareDate: "12/04/20", fileSize: "180 KB", path :{userPath: "/sharedFiles/user.png",iconPath:"/sharedFiles/icon.svg"}
+        id: 3, name: "Design 101", sharedBy: "BJTone", shareDate: "12/04/20", fileSize: "180 KB", path: { userPath: "/images/icon.svg" }
     },
     {
-        id: 4, name: "Design 101", sharedBy: "BJTone", shareDate: "12/04/20", fileSize: "180 KB", path :{userPath: "/sharedFiles/user.png",iconPath:"/sharedFiles/icon.svg"}
+        id: 4, name: "Design 101", sharedBy: "BJTone", shareDate: "12/04/20", fileSize: "180 KB", path: { userPath: "/images/icon.svg" }
     },
     {
-        id: 5, name: "Design 101", sharedBy: "BJTone", shareDate: "12/04/20", fileSize: "180 KB", path :{userPath: "/sharedFiles/user.png",iconPath:"/sharedFiles/icon.svg"}
+        id: 5, name: "Design 101", sharedBy: "BJTone", shareDate: "12/04/20", fileSize: "180 KB", path: { userPath: "/images/icon.svg" }
     },
     {
-        id: 6, name: "Design 101", sharedBy: "BJTone", shareDate: "12/04/20", fileSize: "180 KB", path :{userPath: "/sharedFiles/user.png",iconPath:"/sharedFiles/icon.svg"}
+        id: 6, name: "Design 101", sharedBy: "BJTone", shareDate: "12/04/20", fileSize: "180 KB", path: { userPath: "/images/icon.svg" }
     },
     {
-        id: 7, name: "Design 101", sharedBy: "BJTone", shareDate: "12/04/20", fileSize: "180 KB", path :{userPath: "/sharedFiles/user.png",iconPath:"/sharedFiles/icon.svg"}
+        id: 7, name: "Design 101", sharedBy: "BJTone", shareDate: "12/04/20", fileSize: "180 KB", path: { userPath: "/images/icon.svg" }
     },
     {
-        id: 8, name: "Design 101", sharedBy: "BJTone", shareDate: "12/04/20", fileSize: "180 KB", path :{userPath: "/sharedFiles/user.png",iconPath:"/sharedFiles/icon.svg"}
+        id: 8, name: "Design 101", sharedBy: "BJTone", shareDate: "12/04/20", fileSize: "180 KB", path: { userPath: "/images/icon.svg" }
     },
-  
-   
+
+
 ]
 
 
